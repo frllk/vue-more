33d51bca2683677d1f550caabd2ba4de91e1d8d7
frllk <1402805300@qq.com>
2020-06-20 11:17:00 +0800

    美化按钮===添加动画

--- f-vue-ui/examples/views/Button.vue
+++ f-vue-ui/examples/views/Button.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="about">
     <h1>测试button组件的使用</h1>
+    <my-button color="red" animated>
+      <div slot="hidden">$10000</div>
+      <div slot="visible">鼠标放上来看看</div>
+    </my-button>
+    <my-button color="orange" animated="fade">
+      <div slot="hidden">$10000</div>
+      <div slot="visible">鼠标放上来看看</div>
+    </my-button>
+    <my-button color="green" animated="vertical">
+      <div slot="hidden">$10000</div>
+      <div slot="visible">鼠标放上来看看</div>
+    </my-button>
+    <hr>
     <my-button color="black"></my-button>
     <br>
     <br>
@@ -13,7 +26,7 @@
     <my-button color="blue" size="massive">超大按钮</my-button>
     <br>
     <br>
-    <my-button color="blue" size="massive1">超大按钮</my-button>
+    <my-button color="orange1" size="massive1">超大按钮，测试提示error</my-button>
     <br>
     <br>
     <my-headline></my-headline>
--- f-vue-ui/packages/button/button.vue
+++ f-vue-ui/packages/button/button.vue
@@ -3,8 +3,22 @@
   由于semantic-ui中按钮的大小是通过额外补充一个类，mini...massive来实现的，
   所以，在这里可以通过给外层容器动态绑定class -->
   <div class="ui button" :class="cClass">
+    <!-- 如果传入了animated，则整个视图比较复杂，与不传animated是完全不同的，所以这里直接分两种情况 -->
+    <!-- template：只是逻辑上的容器，并不会产生真实的dom -->
+    <template v-if="animated">
+      <div class="visible content">
+        <!-- 具名插槽 -->
+        <slot name="visible"></slot>
+      </div>
+      <div class="hidden content">
+        <!-- 具名插槽 -->
+        <slot name="hidden"></slot>
+      </div>
+    </template>
     <!-- 默认插槽：如果在使用组件时，传入内容就用传入的，否则显示“按钮” -->
-    <slot>按钮</slot>
+    <template v-else>
+      <slot>按钮</slot>
+    </template>
   </div>
 </template>
 
@@ -31,20 +45,36 @@ export default {
     },
     color: {
       type: String,
+      required: false,
+      validator (val) {
+        // black
+        const colors = ['black', 'yellow', 'green', 'blue', 'orange', 'purple', 'red', 'pink', 'teal']
+        return colors.includes(val)
+      }
+    },
+    // 用它来接收传入的animated属性。有两种类型：boolean，字符串
+    animated: {
+      type: [Boolean, String],
       required: false
     }
   },
   computed: {
     cClass () {
       // 专门用来设置按钮的其他的类
-      const cls = []
-      if (this.size) {
-        cls.push(this.size)
+      const classList = [this.size]
+      if (this.animated) {
+        classList.push('animated')
+        // 如果值是字符串类型，则要额外在补充一个class表示动画的类型
+        if (typeof this.animated === 'string') {
+          classList.push(this.animated)
+        }
       }
       if (this.color) {
-        cls.push(this.color)
+        classList.push(this.color)
       }
-      return cls
+      // vue在绑定class时，是支持直接给数组，所以不用join了，直接返回数组就好
+      // return classList.join(' ')
+      return classList
     }
   },
   data () {
