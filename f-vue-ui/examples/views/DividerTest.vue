0e832253d31e1b7d240add39a86fa54b7ee08195
frllk <1402805300@qq.com>
2020-06-20 14:21:39 +0800

    divider组件===允许用户设置图标

--- f-vue-ui/examples/views/DividerTest.vue
+++ f-vue-ui/examples/views/DividerTest.vue
@@ -1,8 +1,9 @@
 <template>
   <div class=''>
     <h3>对分隔条组件进行测试</h3>
-    <my-divider>标题1</my-divider>
-    <my-divider>标题2</my-divider>
-    <my-divider>标题3</my-divider>
+    <my-divider></my-divider>
+    <my-divider icon="alarm">标题1</my-divider>
+    <my-divider icon="user">标题2</my-divider>
+    <my-divider icon="wifi">标题3</my-divider>
   </div>
 </template>
--- f-vue-ui/packages/divider/divider.vue
+++ f-vue-ui/packages/divider/divider.vue
@@ -1,12 +1,20 @@
 <template>
   <div class='ui divider horizontal'>
-    <!-- 默认分隔线 -->
+    <!-- 是否有图标，取决icon是否有值 -->
+    <i v-if="icon" class="icon" :class="icon"></i>
+    <!-- 默认插槽 -->
     <slot>divider</slot>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'MyDivider'
+  name: 'MyDivider',
+  props: {
+    icon: {
+      type: String,
+      required: false
+    }
+  }
 }
 </script>
