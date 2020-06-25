3334f571561fe55551229dafb0e2f9ef3b49bc8d
frllk <1402805300@qq.com>
2020-06-26 00:48:02 +0800

    headline组件===封装完成

--- f-vue-ui/packages/headline/headline.vue
+++ f-vue-ui/packages/headline/headline.vue
@@ -1,44 +1,3 @@
-<template>
-  <div>
-    <h1 v-if="level===1" class='ui header'>
-      <slot>标题</slot>
-      <div class="sub header">
-        <slot name="sub"></slot>
-      </div>
-    </h1>
-    <h2 v-if="level===2" class='ui header'>
-      <slot>标题</slot>
-      <div class="sub header">
-        <slot name="sub"></slot>
-      </div>
-    </h2>
-    <h3 v-if="level===3" class='ui header'>
-      <slot>标题</slot>
-      <div class="sub header">
-        <slot name="sub"></slot>
-      </div>
-    </h3>
-    <h4 v-if="level===4" class='ui header'>
-      <slot>标题</slot>
-      <div class="sub header">
-        <slot name="sub"></slot>
-      </div>
-    </h4>
-    <h5 v-if="level===5" class='ui header'>
-      <slot>标题</slot>
-      <div class="sub header">
-        <slot name="sub"></slot>
-      </div>
-    </h5>
-    <h6 v-if="level===6" class='ui header'>
-      <slot>标题</slot>
-      <div class="sub header">
-        <slot name="sub"></slot>
-      </div>
-    </h6>
-  </div>
-</template>
-
 <script>
 export default {
   name: 'MyHeadline',
@@ -48,8 +7,23 @@ export default {
       required: false,
       default: 3
     }
+  },
+  render (h) {
+    // 如何在代码中获取插槽的内容？
+    // 答： 通过this.$slots获取所有的插槽信息
+    // 默认插槽： this.$slots.default
+    // 具名插槽： this.$slots.插槽名字
+    console.log(this.$slots)
+    const vList = [this.$slots.default]
+    if (this.$slots.sub) {
+      // 生成一个副标题需要的Vnode
+      const sub = h('div', { class: 'sub header' }, this.$slots.sub[0].children)
+      vList.push(sub)
+    }
+    // 参数1：hX标签
+    // 参数2：hX标签的描述
+    // 参数3：数组。就这个hX标签的子元素
+    return h('h' + this.level, { class: 'ui header' }, vList)
   }
 }
 </script>
-
-<style scoped lang='less'></style>
